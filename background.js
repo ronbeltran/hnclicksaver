025ea404ab1322bb4ffe3b8a9a7b81775c366881
Ronnie Beltran <rbbeltran.09@gmail.com>
2017-09-12 14:16:48 +0800

    Dont open urls if already opened

--- background.js
+++ background.js
@@ -1,6 +1,11 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if(request.message === "open_new_tab"){
-      chrome.tabs.create({"url": request.url});
+      // dont open url if already opened
+      chrome.tabs.query({"url": request.url}, function(tabs){
+        if(tabs.length === 0){
+          chrome.tabs.create({"url": request.url});
+        }
+      });
     }
 });
