d5045a0b1d47041079d087ff1a16f1fcd9d0e754
Ronnie Beltran <rbbeltran.09@gmail.com>
2017-09-12 13:49:14 +0800

    Open story comments page

--- content.js
+++ content.js
@@ -1,7 +1,18 @@
 $(".storylink").click(function(e) {
+  var parent = $(e.target).parent().parent();
+  var commentsUrl = "https://news.ycombinator.com/item?id=" + parent.attr('id');
+
+  // open the link
   chrome.runtime.sendMessage({
     "message": "open_new_tab",
     "url": e.target.href
   });
+
+  // open the comments page
+  chrome.runtime.sendMessage({
+    "message": "open_new_tab",
+    "url": commentsUrl
+  });
+
   e.preventDefault();
-});;
+});
