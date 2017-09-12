046a599026de5c05ef5e5ee09cd6c09b54c809a3
Ronnie Beltran <rbbeltran.09@gmail.com>
2017-09-12 14:31:10 +0800

    Refactor

--- background.js
+++ background.js
@@ -3,8 +3,13 @@ chrome.runtime.onMessage.addListener(
     if(request.message === "open_new_tab"){
       // dont open url if already opened
       chrome.tabs.query({"url": request.url}, function(tabs){
-        if(tabs.length === 0){
-          chrome.tabs.create({"url": request.url});
+        if(tabs.length > 0){
+          chrome.tabs.update(tabs[0].id, {"active": true});
+        } else {
+          chrome.tabs.create({
+            "url": request.url,
+            "active": false
+          });
         }
       });
     }
--- content.js
+++ content.js
@@ -2,17 +2,18 @@ $(".storylink").click(function(e) {
   var parent = $(e.target).closest("tr");
   var commentsUrl = "https://news.ycombinator.com/item?id=" + parent.attr('id');
 
+  // open the comments page
+  chrome.runtime.sendMessage({
+    "message": "open_new_tab",
+    "url": commentsUrl
+  });
+
   // open the link
   chrome.runtime.sendMessage({
     "message": "open_new_tab",
     "url": e.target.href
   });
 
-  // open the comments page
-  chrome.runtime.sendMessage({
-    "message": "open_new_tab",
-    "url": commentsUrl
-  });
 
   e.preventDefault();
 });
